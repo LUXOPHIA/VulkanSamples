e4142709a4df3d49715f5d492b264642fc07718e
luxidea <contact@luxidea.net>
2021-02-19 18:15:36 +0900

    /* */ → (* *)

--- vulkan_core.pas
+++ vulkan_core.pas
@@ -1,16 +1,16 @@
 #ifndef VULKAN_CORE_H_
 #define VULKAN_CORE_H_ 1
 
-/*
+(*
 ** Copyright 2015-2021 The Khronos Group Inc.
 **
 ** SPDX-License-Identifier: Apache-2.0
-*/
+*)
 
-/*
+(*
 ** This header is generated from the Khronos Vulkan XML API Registry.
 **
-*/
+*)
 
 
 #ifdef __cplusplus
