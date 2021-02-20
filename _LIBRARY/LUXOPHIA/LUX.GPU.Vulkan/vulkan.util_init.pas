4cb377b90546d9a84d49d1be95c7bdcb64da2c8e
luxidea <contact@luxidea.net>
2021-02-20 17:32:09 +0900

    init_device_extension_properties, init_enumerate_device, destroy_instance 関数を追加。

--- _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/vulkan.util_init.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/vulkan.util_init.pas
@@ -39,10 +39,13 @@ uses vulkan_core,
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
 function init_global_extension_properties( var layer_props_:T_layer_properties ) :VkResult;
-
 function init_global_layer_properties( var info_:T_sample_info ) :VkResult;
 
-function init_instance( var info:T_sample_info; const app_short_name:P_char ) :VkResult;
+function init_instance( var info_:T_sample_info; const app_short_name_:P_char ) :VkResult;
+
+function init_device_extension_properties( var info_:T_sample_info; var layer_props_:T_layer_properties ) :VkResult;
+function init_enumerate_device( var info_:T_sample_info; gpu_count_:T_uint32_t = 1 ) :VkResult;
+procedure destroy_instance( var info_:T_sample_info );
 
 implementation //############################################################### ■
 
@@ -130,16 +133,18 @@ begin
      vk_props := nil;
 end;
 
-function init_instance( var info:T_sample_info; const app_short_name:P_char ) :VkResult;
+//------------------------------------------------------------------------------
+
+function init_instance( var info_:T_sample_info; const app_short_name_:P_char ) :VkResult;
 var
    app_info  :VkApplicationInfo;
    inst_info :VkInstanceCreateInfo;
 begin
     app_info.sType              := VK_STRUCTURE_TYPE_APPLICATION_INFO;
     app_info.pNext              := nil;
-    app_info.pApplicationName   := app_short_name;
+    app_info.pApplicationName   := app_short_name_;
     app_info.applicationVersion := 1;
-    app_info.pEngineName        := app_short_name;
+    app_info.pEngineName        := app_short_name_;
     app_info.engineVersion      := 1;
     app_info.apiVersion         := VK_API_VERSION_1_0;
 
@@ -147,15 +152,71 @@ begin
     inst_info.pNext                    := nil;
     inst_info.flags                    := 0;
     inst_info.pApplicationInfo         := @app_info;
-    inst_info.enabledLayerCount        := Length( info.instance_layer_names );
-    if Length( info.instance_layer_names ) > 0
-    then inst_info.ppEnabledLayerNames := @info.instance_layer_names[0]
+    inst_info.enabledLayerCount        := Length( info_.instance_layer_names );
+    if Length( info_.instance_layer_names ) > 0
+    then inst_info.ppEnabledLayerNames := @info_.instance_layer_names[0]
     else inst_info.ppEnabledLayerNames := nil;
-    inst_info.enabledExtensionCount    := Length( info.instance_extension_names );
-    inst_info.ppEnabledExtensionNames  := @info.instance_extension_names[ 0 ];
+    inst_info.enabledExtensionCount    := Length( info_.instance_extension_names );
+    inst_info.ppEnabledExtensionNames  := @info_.instance_extension_names[ 0 ];
 
-    Result := vkCreateInstance( @inst_info, nil, @info.inst );
+    Result := vkCreateInstance( @inst_info, nil, @info_.inst );
     Assert( Result = VK_SUCCESS );
+end;
+
+//------------------------------------------------------------------------------
+
+function init_device_extension_properties( var info_:T_sample_info; var layer_props_:T_layer_properties ) :VkResult;
+var
+   device_extensions      :P_VkExtensionProperties;
+   device_extension_count :T_uint32_t;
+   layer_name             :P_char;
+begin
+     layer_name := layer_props_.properties.layerName;
+
+     repeat
+           Result := vkEnumerateDeviceExtensionProperties( info_.gpus[ 0 ], layer_name, @device_extension_count, nil );
+           if Result <> VK_SUCCESS then Exit;
+
+           if device_extension_count = 0 then Exit( VK_SUCCESS );
+
+           SetLength( layer_props_.device_extensions, device_extension_count );
+           device_extensions := @layer_props_.device_extensions[ 0 ];
+           Result := vkEnumerateDeviceExtensionProperties( info_.gpus[ 0 ], layer_name, @device_extension_count, device_extensions );
+
+     until Result <> VK_INCOMPLETE;
+end;
+
+function init_enumerate_device( var info_:T_sample_info; gpu_count_:T_uint32_t = 1 ) :VkResult;
+var
+   req_count :T_uint32_t;
+   I         :Integer;
+begin
+     req_count := gpu_count_;
+     {Result := }vkEnumeratePhysicalDevices( info_.inst, @gpu_count_, nil );
+     Assert( gpu_count_ > 0 );
+     SetLength( info_.gpus, gpu_count_ );
+
+     Result := vkEnumeratePhysicalDevices( info_.inst, @gpu_count_, @info_.gpus[ 0 ] );
+     Assert( ( Result = VK_SUCCESS ) and ( gpu_count_ >= req_count ) );
+
+     vkGetPhysicalDeviceQueueFamilyProperties( info_.gpus[ 0 ], @info_.queue_family_count, nil );
+     Assert( info_.queue_family_count >= 1 );
+
+     SetLength( info_.queue_props, info_.queue_family_count );
+     vkGetPhysicalDeviceQueueFamilyProperties( info_.gpus[ 0 ], @info_.queue_family_count, @info_.queue_props[ 0 ] );
+     Assert( info_.queue_family_count >= 1 );
+
+     (* This is as good a place as any to do this *)
+     vkGetPhysicalDeviceMemoryProperties( info_.gpus[ 0 ], @info_.memory_properties );
+     vkGetPhysicalDeviceProperties( info_.gpus[ 0 ], @info_.gpu_props );
+     (* query device extensions for enabled layers *)
+     for I := 0 to Length( info_.instance_layer_properties )-1
+     do init_device_extension_properties( info_, info_.instance_layer_properties[ I ] );
+end;
+
+procedure destroy_instance( var info_:T_sample_info );
+begin
+     vkDestroyInstance( info_.inst, nil );
 end;
 
 end. //######################################################################### ■
