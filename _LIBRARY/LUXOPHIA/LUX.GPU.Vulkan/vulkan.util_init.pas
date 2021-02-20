21a8713b814f14b21398df41d2f982718f966ff0
luxidea <contact@luxidea.net>
2021-02-20 16:12:11 +0900

    util.pas, util_init.pas を追加。

---
+++ _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/vulkan.util_init.pas
@@ -0,0 +1,149 @@
+﻿unit vulkan.util_init;
+
+(*
+ * Vulkan Samples
+ *
+ * Copyright (C) 2015-2020 Valve Corporation
+ * Copyright (C) 2015-2020 LunarG, Inc.
+ * Copyright (C) 2015-2020 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+interface //#################################################################### ■
+
+uses vulkan_core,
+     vulkan.util,
+     LUX.Code.C;
+
+//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+function init_global_extension_properties( var layer_props_:T_layer_properties ) :VkResult;
+
+function init_global_layer_properties( var info_:T_sample_info ) :VkResult;
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+function init_global_extension_properties( var layer_props_:T_layer_properties ) :VkResult;
+var
+   instance_extensions      :P_VkExtensionProperties;
+   instance_extension_count :T_uint32_t;
+   layer_name               :P_char;
+begin
+     layer_name := layer_props_.properties.layerName;
+
+     repeat
+           Result := vkEnumerateInstanceExtensionProperties( layer_name, @instance_extension_count, nil );
+           if Result <> VK_SUCCESS then Exit;
+
+           if instance_extension_count = 0 then Exit( VK_SUCCESS );
+
+           SetLength( layer_props_.instance_extensions, instance_extension_count );
+           instance_extensions := @layer_props_.instance_extensions[ 0 ];
+           Result := vkEnumerateInstanceExtensionProperties( layer_name, @instance_extension_count, instance_extensions );
+
+     until Result <> VK_INCOMPLETE;
+end;
+
+function init_global_layer_properties( var info_:T_sample_info ) :VkResult;
+var
+   instance_layer_count :T_uint32_t;
+   vk_props             :TArray<VkLayerProperties>;
+   i                    :T_uint32_t;
+   layer_props          :T_layer_properties;
+begin
+     {$IFDEF Android }
+     // This place is the first place for samples to use Vulkan APIs.
+     // Here, we are going to open Vulkan.so on the device and retrieve function pointers using
+     // vulkan_wrapper helper.
+     if not InitVulkan then
+     begin
+          LOGE(' Failied initializing Vulkan APIs!' );
+
+          Exit( VK_ERROR_INITIALIZATION_FAILED );
+     end;
+     LOGI( 'Loaded Vulkan APIs.' );
+     {$ENDIF}
+
+     (*
+      * It's possible, though very rare, that the number of
+      * instance layers could change. For example, installing something
+      * could include new layers that the loader would pick up
+      * between the initial query for the count and the
+      * request for VkLayerProperties. The loader indicates that
+      * by returning a VK_INCOMPLETE status and will update the
+      * the count parameter.
+      * The count parameter will be updated with the number of
+      * entries loaded into the data pointer - in case the number
+      * of layers went down or is smaller than the size given.
+      *)
+     repeat
+           Result := vkEnumerateInstanceLayerProperties( @instance_layer_count, nil );
+           if Result <> VK_SUCCESS then Exit;
+
+           if instance_layer_count = 0 then Exit( VK_SUCCESS );
+
+           SetLength( vk_props, instance_layer_count );
+
+           Result := vkEnumerateInstanceLayerProperties( @instance_layer_count, @vk_props[ 0 ] );
+
+        until Result <> VK_INCOMPLETE;
+
+     (*
+      * Now gather the extension list for each instance layer.
+      *)
+     for i := 0 to instance_layer_count-1 do
+     begin
+          layer_props.properties := vk_props[ i ];
+          Result := init_global_extension_properties( layer_props );
+          if Result <> VK_SUCCESS then Exit;
+          info_.instance_layer_properties := info_.instance_layer_properties + [ layer_props ];
+     end;
+     vk_props := nil;
+end;
+
+end. //######################################################################### ■
