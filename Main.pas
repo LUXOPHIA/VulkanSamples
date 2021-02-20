09aa31cf85fd819796d5e1fbcd656bd6bb06ae82
luxidea <contact@luxidea.net>
2021-02-20 16:14:15 +0900

    01-init_instance

--- Main.pas
+++ Main.pas
@@ -4,7 +4,9 @@ interface //####################################################################
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  vulkan_core,
+  vulkan.util, vulkan.util_init;
 
 type
   TForm1 = class(TForm)
@@ -23,9 +25,51 @@ implementation //###############################################################
 
 {$R *.fmx}
 
+// https://github.com/LunarG/VulkanSamples/tree/master/API-Samples/01-init_instance
+
+const APP_SHORT_NAME = 'vulkansamples_instance';
+
 procedure TForm1.FormCreate(Sender: TObject);
+var
+   info      :T_sample_info;
+   app_info  :VkApplicationInfo;
+   inst_info :VkInstanceCreateInfo;
+   inst      :VkInstance;
+   res       :VkResult;
 begin
-     /////
+     Caption := APP_SHORT_NAME;
+
+     init_global_layer_properties( info );
+
+     (* VULKAN_KEY_START *)
+
+     // initialize the VkApplicationInfo structure
+     app_info.sType              := VK_STRUCTURE_TYPE_APPLICATION_INFO;
+     app_info.pNext              := nil;
+     app_info.pApplicationName   := APP_SHORT_NAME;
+     app_info.applicationVersion := 1;
+     app_info.pEngineName        := APP_SHORT_NAME;
+     app_info.engineVersion      := 1;
+     app_info.apiVersion         := VK_API_VERSION_1_0;
+
+     // initialize the VkInstanceCreateInfo structure
+     inst_info.sType                   := VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+     inst_info.pNext                   := nil;
+     inst_info.flags                   := 0;
+     inst_info.pApplicationInfo        := @app_info;
+     inst_info.enabledExtensionCount   := 0;
+     inst_info.ppEnabledExtensionNames := nil;
+     inst_info.enabledLayerCount       := 0;
+     inst_info.ppEnabledLayerNames     := nil;
+
+     res := vkCreateInstance( @inst_info, nil, @inst );
+
+     Assert( res <> VK_ERROR_INCOMPATIBLE_DRIVER, 'cannot find a compatible Vulkan ICD' );
+     Assert( res = VK_SUCCESS, 'unknown error' );
+
+     vkDestroyInstance( inst, nil );
+
+     (* VULKAN_KEY_END *)
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
