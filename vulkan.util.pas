c2118c4cbe02d90b0ec1af3f8a86d5800a506fd0
luxidea <contact@luxidea.net>
2021-02-20 23:59:27 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/' changes from 9b9adee..39564ef
    39564ef Merge branch 'vulkan_win32-169'
    554d80d no message
    74fed6d no message
    2406fd7 no message
    fe28a3b no message
    fb1367e no message
    59cc4ee vulkan_win32.h (1.2.169)
    39d48ca init_queue_family_index, init_device, destroy_device 関数を追加。
    93ae53c init_device_extension_properties, init_enumerate_device, destroy_instance 関数を追加。
    5971fb2 init_instance 関数を追加。
    a7d6113 no message
    2fe4c85 util.pas, util_init.pas を追加。
    59c5085 01-init_instance
    9b0ebe9 char → T_char
    b08f98f no message
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan
    git-subtree-split: 39564ef870c6f545e9d55e7c15d7a841a589815f

---
+++ vulkan.util.pas
@@ -0,0 +1,214 @@
+﻿unit vulkan.util;
+
+(*
+ * Vulkan Samples
+ *
+ * Copyright (C) 2015-2016 Valve Corporation
+ * Copyright (C) 2015-2016 LunarG, Inc.
+ * Copyright (C) 2015-2016 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+interface //#################################################################### ■
+
+uses vulkan_core,
+     LUX.Code.C,
+     LUX.D1, LUX.D2, LUX.D2x2, LUX.D3, LUX.D3x3, LUX.D4x4;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% T_texture_object
+     (*
+      * structure to track all objects related to a texture.
+      *)
+     T_texture_object = record
+       sampler       :VkSampler;
+
+       image         :VkImage;
+       imageLayout   :VkImageLayout;
+
+       needs_staging :T_bool;
+       buffer        :VkBuffer;
+       buffer_size   :VkDeviceSize;
+
+       image_memory  :VkDeviceMemory;
+       buffer_memory :VkDeviceMemory;
+       view          :VkImageView;
+       tex_width     :T_int32_t;
+       tex_height    :T_int32_t;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% T_swap_chain_buffer
+     (*
+      * Keep each of our swap chain buffers' image, command buffer and view in one
+      * spot
+      *)
+     T_swap_chain_buffer = record
+       image :VkImage;
+       view  :VkImageView;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% T_layer_properties
+     (*
+      * A layer can expose extensions, keep track of those
+      * extensions here.
+      *)
+     T_layer_properties = record
+       properties          :VkLayerProperties;
+       instance_extensions :TArray<VkExtensionProperties>;
+       device_extensions   :TArray<VkExtensionProperties>;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% T_sample_info
+     (*
+      * Structure for tracking information used / created / modified
+      * by utility functions.
+      *)
+     T_sample_info = record
+     private
+       const APP_NAME_STR_LEN = 80;
+     public
+     {$IFDEF MSWINDOWS }
+       connection :T_HINSTANCE;                                // hInstance - Windows Instance
+       name       :array [ 0..APP_NAME_STR_LEN-1 ] of T_char;  // Name to put on the window/icon
+       window     :T_HWND;                                     // hWnd - window handle
+     {$ELSEIF Defined( VK_USE_PLATFORM_METAL_EXT ) }
+       caMetalLayer :P_void;
+     {$ELSEIF Android }
+       fpCreateAndroidSurfaceKHR :PFN_vkCreateAndroidSurfaceKHR;
+     {$ELSEIF Defined( VK_USE_PLATFORM_WAYLAND_KHR ) }
+       display       :P_wl_display;
+       registry      :P_wl_registry ;
+       compositor    :P_wl_compositor;
+       window        :P_wl_surface;
+       shell         :P_wl_shell;
+       shell_surface :P_wl_shell_surface;
+     {$ELSE}
+       connection            :P_xcb_connection_t;
+       screen                :P_xcb_screen_t;
+       window                :T_xcb_window_t;
+       atom_wm_delete_window :P_xcb_intern_atom_reply_t;
+     {$ENDIF} // MSWINDOWS
+       Surface            :VkSurfaceKHR ;
+       Prepared           :T_bool;
+       Use_staging_buffer :T_bool;
+       Save_images        :T_bool;
+
+       instance_layer_names          :TArray<P_char>;
+       instance_extension_names      :TArray<P_char>;
+       instance_layer_properties     :TArray<T_layer_properties>;
+       instance_extension_properties :TArray<VkExtensionProperties> ;
+       inst                          :VkInstance;
+
+       device_extension_names      :TArray<P_char>;
+       device_extension_properties :TArray<VkExtensionProperties>;
+       gpus                        :TArray<VkPhysicalDevice>;
+       device                      :VkDevice;
+       graphics_queue              :VkQueue;
+       present_queue               :VkQueue;
+       graphics_queue_family_index :T_uint32_t;
+       present_queue_family_index  :T_uint32_t;
+       gpu_props                   :VkPhysicalDeviceProperties;
+       queue_props                 :TArray<VkQueueFamilyProperties>;
+       memory_properties           :VkPhysicalDeviceMemoryProperties;
+
+       framebuffers  :P_VkFramebuffer;
+       width         :T_int;
+       height        :T_int;
+       format        :VkFormat;
+
+       swapchainImageCount    :T_uint32_t;
+       swap_chain             :VkSwapchainKHR;
+       buffers                :TArray<T_swap_chain_buffer>;
+       imageAcquiredSemaphore :VkSemaphore;
+
+       cmd_pool :VkCommandPool;
+
+       depth :record
+                format :VkFormat;
+                image  :VkImage;
+                mem    :VkDeviceMemory;
+                view   :VkImageView;
+              end;
+
+       textures :TArray<T_texture_object>;
+
+       uniform_data :record
+                      buf         :VkBuffer;
+                      mem         :VkDeviceMemory;
+                      buffer_info :VkDescriptorBufferInfo;
+                    end;
+
+       texture_data :record
+                       image_info :VkDescriptorImageInfo;
+                     end;
+
+       vertex_buffer :record
+                        buf         :VkBuffer;
+                        mem         :VkDeviceMemory;
+                        buffer_info :VkDescriptorBufferInfo;
+                      end;
+
+       vi_binding    :VkVertexInputBindingDescription;
+       vi_attribs    :array [ 0..2-1 ] of VkVertexInputAttributeDescription;
+
+       Projection :TSingleM4;
+       View       :TSingleM4;
+       Model      :TSingleM4;
+       Clip       :TSingleM4;
+       MVP        :TSingleM4;
+
+       cmd             :VkCommandBuffer; // Buffer for initialization commands
+       pipeline_layout :VkPipelineLayout;
+       desc_layout     :TArray<VkDescriptorSetLayout>;
+       pipelineCache   :VkPipelineCache;
+       render_pass     :VkRenderPass;
+       pipeline        :VkPipeline;
+
+       shaderStages : array [ 0..2-1 ] of VkPipelineShaderStageCreateInfo;
+
+       desc_pool :VkDescriptorPool;
+       desc_set  :TArray<VkDescriptorSet>;
+
+       dbgCreateDebugReportCallback  :PFN_vkCreateDebugReportCallbackEXT;
+       dbgDestroyDebugReportCallback :PFN_vkDestroyDebugReportCallbackEXT;
+       dbgBreakCallback              :PFN_vkDebugReportMessageEXT;
+       debug_report_callbacks        :TArray<VkDebugReportCallbackEXT>;
+
+       current_buffer     :T_uint32_t;
+       queue_family_count :T_uint32_t;
+
+       viewport :VkViewport;
+       scissor  :VkRect2D;
+     end;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+end. //######################################################################### ■
---
+++ vulkan.util_init.pas
@@ -0,0 +1,294 @@
+﻿unit vulkan.util_init;
+
+(*
+ * Vulkan Samples
+ *
+ * Copyright (C) 2015-2020 Valve Corporation
+ * Copyright (C) 2015-2020 LunarG, Inc.
+ * Copyright (C) 2015-2020 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+interface //#################################################################### ■
+
+uses vulkan_core,
+     vulkan.util,
+     LUX.Code.C;
+
+//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+function init_global_extension_properties( var layer_props_:T_layer_properties ) :VkResult;
+function init_global_layer_properties( var info_:T_sample_info ) :VkResult;
+
+function init_instance( var info_:T_sample_info; const app_short_name_:P_char ) :VkResult;
+
+function init_device_extension_properties( var info_:T_sample_info; var layer_props_:T_layer_properties ) :VkResult;
+function init_enumerate_device( var info_:T_sample_info; gpu_count_:T_uint32_t = 1 ) :VkResult;
+procedure destroy_instance( var info_:T_sample_info );
+
+procedure init_queue_family_index( var info_:T_sample_info );
+function init_device( var info_:T_sample_info ) :VkResult;
+procedure destroy_device( var info_:T_sample_info );
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+function init_global_extension_properties( var layer_props_:T_layer_properties ) :VkResult;
+var
+   instance_extensions      :P_VkExtensionProperties;
+   instance_extension_count :T_uint32_t;
+   layer_name               :P_char;
+begin
+     layer_name := layer_props_.properties.layerName;
+
+     repeat
+           Result := vkEnumerateInstanceExtensionProperties( layer_name, @instance_extension_count, nil );
+           if Result <> VK_SUCCESS then Exit;
+
+           if instance_extension_count = 0 then Exit( VK_SUCCESS );
+
+           SetLength( layer_props_.instance_extensions, instance_extension_count );
+           instance_extensions := @layer_props_.instance_extensions[ 0 ];
+           Result := vkEnumerateInstanceExtensionProperties( layer_name, @instance_extension_count, instance_extensions );
+
+     until Result <> VK_INCOMPLETE;
+end;
+
+function init_global_layer_properties( var info_:T_sample_info ) :VkResult;
+var
+   instance_layer_count :T_uint32_t;
+   vk_props             :TArray<VkLayerProperties>;
+   i                    :T_uint32_t;
+   layer_props          :T_layer_properties;
+begin
+     {$IFDEF Android }
+     // This place is the first place for samples to use Vulkan APIs.
+     // Here, we are going to open Vulkan.so on the device and retrieve function pointers using
+     // vulkan_wrapper helper.
+     if not InitVulkan then
+     begin
+          LOGE(' Failied initializing Vulkan APIs!' );
+
+          Exit( VK_ERROR_INITIALIZATION_FAILED );
+     end;
+     LOGI( 'Loaded Vulkan APIs.' );
+     {$ENDIF}
+
+     (*
+      * It's possible, though very rare, that the number of
+      * instance layers could change. For example, installing something
+      * could include new layers that the loader would pick up
+      * between the initial query for the count and the
+      * request for VkLayerProperties. The loader indicates that
+      * by returning a VK_INCOMPLETE status and will update the
+      * the count parameter.
+      * The count parameter will be updated with the number of
+      * entries loaded into the data pointer - in case the number
+      * of layers went down or is smaller than the size given.
+      *)
+     repeat
+           Result := vkEnumerateInstanceLayerProperties( @instance_layer_count, nil );
+           if Result <> VK_SUCCESS then Exit;
+
+           if instance_layer_count = 0 then Exit( VK_SUCCESS );
+
+           SetLength( vk_props, instance_layer_count );
+
+           Result := vkEnumerateInstanceLayerProperties( @instance_layer_count, @vk_props[ 0 ] );
+
+        until Result <> VK_INCOMPLETE;
+
+     (*
+      * Now gather the extension list for each instance layer.
+      *)
+     for i := 0 to instance_layer_count-1 do
+     begin
+          layer_props.properties := vk_props[ i ];
+          Result := init_global_extension_properties( layer_props );
+          if Result <> VK_SUCCESS then Exit;
+          info_.instance_layer_properties := info_.instance_layer_properties + [ layer_props ];
+     end;
+     vk_props := nil;
+end;
+
+//------------------------------------------------------------------------------
+
+function init_instance( var info_:T_sample_info; const app_short_name_:P_char ) :VkResult;
+var
+   app_info  :VkApplicationInfo;
+   inst_info :VkInstanceCreateInfo;
+begin
+    app_info.sType              := VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pNext              := nil;
+    app_info.pApplicationName   := app_short_name_;
+    app_info.applicationVersion := 1;
+    app_info.pEngineName        := app_short_name_;
+    app_info.engineVersion      := 1;
+    app_info.apiVersion         := VK_API_VERSION_1_0;
+
+    inst_info.sType                    := VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    inst_info.pNext                    := nil;
+    inst_info.flags                    := 0;
+    inst_info.pApplicationInfo         := @app_info;
+    inst_info.enabledLayerCount        := Length( info_.instance_layer_names );
+    if Length( info_.instance_layer_names ) > 0
+    then inst_info.ppEnabledLayerNames := @info_.instance_layer_names[0]
+    else inst_info.ppEnabledLayerNames := nil;
+    inst_info.enabledExtensionCount    := Length( info_.instance_extension_names );
+    inst_info.ppEnabledExtensionNames  := @info_.instance_extension_names[ 0 ];
+
+    Result := vkCreateInstance( @inst_info, nil, @info_.inst );
+    Assert( Result = VK_SUCCESS );
+end;
+
+//------------------------------------------------------------------------------
+
+function init_device_extension_properties( var info_:T_sample_info; var layer_props_:T_layer_properties ) :VkResult;
+var
+   device_extensions      :P_VkExtensionProperties;
+   device_extension_count :T_uint32_t;
+   layer_name             :P_char;
+begin
+     layer_name := layer_props_.properties.layerName;
+
+     repeat
+           Result := vkEnumerateDeviceExtensionProperties( info_.gpus[ 0 ], layer_name, @device_extension_count, nil );
+           if Result <> VK_SUCCESS then Exit;
+
+           if device_extension_count = 0 then Exit( VK_SUCCESS );
+
+           SetLength( layer_props_.device_extensions, device_extension_count );
+           device_extensions := @layer_props_.device_extensions[ 0 ];
+           Result := vkEnumerateDeviceExtensionProperties( info_.gpus[ 0 ], layer_name, @device_extension_count, device_extensions );
+
+     until Result <> VK_INCOMPLETE;
+end;
+
+function init_enumerate_device( var info_:T_sample_info; gpu_count_:T_uint32_t = 1 ) :VkResult;
+var
+   req_count :T_uint32_t;
+   I         :Integer;
+begin
+     req_count := gpu_count_;
+     {Result := }vkEnumeratePhysicalDevices( info_.inst, @gpu_count_, nil );
+     Assert( gpu_count_ > 0 );
+     SetLength( info_.gpus, gpu_count_ );
+
+     Result := vkEnumeratePhysicalDevices( info_.inst, @gpu_count_, @info_.gpus[ 0 ] );
+     Assert( ( Result = VK_SUCCESS ) and ( gpu_count_ >= req_count ) );
+
+     vkGetPhysicalDeviceQueueFamilyProperties( info_.gpus[ 0 ], @info_.queue_family_count, nil );
+     Assert( info_.queue_family_count >= 1 );
+
+     SetLength( info_.queue_props, info_.queue_family_count );
+     vkGetPhysicalDeviceQueueFamilyProperties( info_.gpus[ 0 ], @info_.queue_family_count, @info_.queue_props[ 0 ] );
+     Assert( info_.queue_family_count >= 1 );
+
+     (* This is as good a place as any to do this *)
+     vkGetPhysicalDeviceMemoryProperties( info_.gpus[ 0 ], @info_.memory_properties );
+     vkGetPhysicalDeviceProperties( info_.gpus[ 0 ], @info_.gpu_props );
+     (* query device extensions for enabled layers *)
+     for I := 0 to Length( info_.instance_layer_properties )-1
+     do init_device_extension_properties( info_, info_.instance_layer_properties[ I ] );
+end;
+
+procedure destroy_instance( var info_:T_sample_info );
+begin
+     vkDestroyInstance( info_.inst, nil );
+end;
+
+//------------------------------------------------------------------------------
+
+procedure init_queue_family_index( var info_:T_sample_info );
+var
+   found :T_bool;
+   i     :T_unsigned_int;
+begin
+     (* This routine simply finds a graphics queue for a later vkCreateDevice,
+      * without consideration for which queue family can present an image.
+      * Do not use this if your intent is to present later in your sample,
+      * instead use the init_connection, init_window, init_swapchain_extension,
+      * init_device call sequence to get a graphics and present compatible queue
+      * family
+      *)
+
+     vkGetPhysicalDeviceQueueFamilyProperties( info_.gpus[ 0 ], @info_.queue_family_count, nil );
+     Assert( info_.queue_family_count >= 1 );
+
+     SetLength( info_.queue_props, info_.queue_family_count );
+     vkGetPhysicalDeviceQueueFamilyProperties( info_.gpus[ 0 ], @info_.queue_family_count, @info_.queue_props[ 0 ] );
+     Assert( info_.queue_family_count >= 1 );
+
+     found := False;
+     for i := 0 to info_.queue_family_count-1 do
+     begin
+          if ( info_.queue_props[ i ].queueFlags and VkQueueFlags( VK_QUEUE_GRAPHICS_BIT ) ) > 0 then
+          begin
+               info_.graphics_queue_family_index := i;
+               found := True;
+               Break;
+          end;
+     end;
+     Assert( found );
+end;
+
+function init_device( var info_:T_sample_info ) :VkResult;
+var
+   queue_info       :VkDeviceQueueCreateInfo;
+   queue_priorities :array [ 0..0 ] of T_float;
+   device_info      :VkDeviceCreateInfo;
+begin
+     queue_priorities[ 0 ] := 0;
+     queue_info.sType            := VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+     queue_info.pNext            := nil;
+     queue_info.queueCount       := 1;
+     queue_info.pQueuePriorities := @queue_priorities[ 0 ];
+     queue_info.queueFamilyIndex := info_.graphics_queue_family_index;
+
+     device_info.sType                        := VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+     device_info.pNext                        := nil;
+     device_info.queueCreateInfoCount         := 1;
+     device_info.pQueueCreateInfos            := @queue_info;
+     device_info.enabledExtensionCount        := Length( info_.device_extension_names );
+     if device_info.enabledExtensionCount > 0
+     then device_info.ppEnabledExtensionNames := @info_.device_extension_names[ 0 ]
+     else device_info.ppEnabledExtensionNames := nil;
+     device_info.pEnabledFeatures             := nil;
+
+     Result := vkCreateDevice( info_.gpus[ 0 ], @device_info, nil, @info_.device );
+     Assert( Result = VK_SUCCESS );
+end;
+
+procedure destroy_device( var info_:T_sample_info );
+begin
+     vkDeviceWaitIdle( info_.device );
+     vkDestroyDevice( info_.device, nil );
+end;
+
+end. //######################################################################### ■
