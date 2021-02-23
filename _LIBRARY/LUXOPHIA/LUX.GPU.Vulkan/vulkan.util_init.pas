b0b66c6f7c9cf09371db04983400b733c722cd6c
luxidea <contact@luxidea.net>
2021-02-24 02:47:41 +0900

    no message

--- _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/vulkan.util_init.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/vulkan.util_init.pas
@@ -61,16 +61,16 @@ procedure destroy_device( var info_:T_sample_info );
 
 //////////////////////////////////////////////////////////////////////////////// 05-init_swapchain
 
-procedure init_instance_extension_names( var info:T_sample_info );
-procedure init_device_extension_names( var info:T_sample_info );
-procedure init_window_size( var info:T_sample_info; default_width,default_height:UInt32 );
-procedure init_connection( var info:T_sample_info );
-procedure init_window( var info:T_sample_info );
-procedure destroy_window( var info:T_sample_info );
+procedure init_instance_extension_names( var info_:T_sample_info );
+procedure init_device_extension_names( var info_:T_sample_info );
+procedure init_window_size( var info_:T_sample_info; default_width_,default_height_:UInt32 );
+procedure init_connection( var info_:T_sample_info );
+procedure init_window( var info_:T_sample_info );
+procedure destroy_window( var info_:T_sample_info );
 
 //////////////////////////////////////////////////////////////////////////////// 06-init_depth_buffer
 
-procedure init_swapchain_extension( var info:T_sample_info );
+procedure init_swapchain_extension( var info_:T_sample_info );
 
 implementation //############################################################### ■
 
@@ -307,24 +307,24 @@ end;
 
 //////////////////////////////////////////////////////////////////////////////// 05-init_swapchain
 
-procedure init_instance_extension_names( var info:T_sample_info );
+procedure init_instance_extension_names( var info_:T_sample_info );
 begin
-     info.instance_extension_names := info.instance_extension_names + [ VK_KHR_SURFACE_EXTENSION_NAME         ];
-     info.instance_extension_names := info.instance_extension_names + [ VK_KHR_WIN32_SURFACE_EXTENSION_NAME   ];
+     info_.instance_extension_names := info_.instance_extension_names + [ VK_KHR_SURFACE_EXTENSION_NAME         ];
+     info_.instance_extension_names := info_.instance_extension_names + [ VK_KHR_WIN32_SURFACE_EXTENSION_NAME   ];
 end;
 
-procedure init_device_extension_names( var info:T_sample_info );
+procedure init_device_extension_names( var info_:T_sample_info );
 begin
-     info.device_extension_names := info.device_extension_names + [ VK_KHR_SWAPCHAIN_EXTENSION_NAME ];
+     info_.device_extension_names := info_.device_extension_names + [ VK_KHR_SWAPCHAIN_EXTENSION_NAME ];
 end;
 
-procedure init_window_size( var info:T_sample_info; default_width,default_height:UInt32 );
+procedure init_window_size( var info_:T_sample_info; default_width_,default_height_:UInt32 );
 begin
-     info.width  := default_width;
-     info.height := default_height;
+     info_.width  := default_width_;
+     info_.height := default_height_;
 end;
 
-procedure init_connection( var info:T_sample_info );
+procedure init_connection( var info_:T_sample_info );
 begin
 
 end;
@@ -351,16 +351,16 @@ begin
      Result := DefWindowProc( hWnd, uMsg, wParam, lParam );
 end;
 
-procedure init_window( var info:T_sample_info );
+procedure init_window( var info_:T_sample_info );
 var
    win_class :WNDCLASSEX;
    wr        :TRect;
 begin
-     Assert( info.width  > 0 );
-     Assert( info.height > 0 );
+     Assert( info_.width  > 0 );
+     Assert( info_.height > 0 );
 
-     info.connection := GetModuleHandle( nil );
-     info.name       := 'Sample';
+     info_.connection := GetModuleHandle( nil );
+     info_.name       := 'Sample';
 
      // Initialize the window class structure:
      win_class.cbSize        := SizeOf( WNDCLASSEX );
@@ -368,12 +368,12 @@ begin
      win_class.lpfnWndProc   := @WndProc;
      win_class.cbClsExtra    := 0;
      win_class.cbWndExtra    := 0;
-     win_class.hInstance     := info.connection;  // hInstance
+     win_class.hInstance     := info_.connection;  // hInstance
      win_class.hIcon         := LoadIcon( 0, IDI_APPLICATION );
      win_class.hCursor       := LoadCursor( 0, IDC_ARROW );
      win_class.hbrBackground := HBRUSH( GetStockObject( WHITE_BRUSH ) );
      win_class.lpszMenuName  := nil;
-     win_class.lpszClassName := LPCWSTR( WideString( info.name ) );
+     win_class.lpszClassName := LPCWSTR( WideString( info_.name ) );
      win_class.hIconSm       := LoadIcon( 0, IDI_WINLOGO );
      // Register window class:
      if RegisterClassEx( win_class ) = 0 then
@@ -383,32 +383,32 @@ begin
           RunError( 1 );
      end;
      // Create window with the registered class:
-     wr := TRect.Create( 0, 0, info.width, info.height );
+     wr := TRect.Create( 0, 0, info_.width, info_.height );
      AdjustWindowRect( wr, WS_OVERLAPPEDWINDOW, False );
-     info.window := CreateWindowEx( 0,
-                                    LPCWSTR( WideString( info.name ) ),               // class name
-                                    LPCWSTR( WideString( info.name ) ),               // app name
+     info_.window := CreateWindowEx( 0,
+                                    LPCWSTR( WideString( info_.name ) ),              // class name
+                                    LPCWSTR( WideString( info_.name ) ),              // app name
                                     WS_OVERLAPPEDWINDOW or WS_VISIBLE or WS_SYSMENU,  // window style
                                     100, 100,                                         // x/y coords
                                     wr.right - wr.left,                               // width
                                     wr.bottom - wr.top,                               // height
                                     0,                                                // handle to parent
                                     0,                                                // handle to menu
-                                    info.connection,                                  // hInstance
+                                    info_.connection,                                 // hInstance
                                     nil );                                            // no extra parameters
-     if info.window = 0 then
+     if info_.window = 0 then
      begin
           // It didn't work, so try to give a useful error:
           Log.d( 'Cannot create a window in which to draw!' );
           RunError( 1 );
      end;
-     SetWindowLongPtr( info.window, GWLP_USERDATA, LONG_PTR( @info ) );
+     SetWindowLongPtr( info_.window, GWLP_USERDATA, LONG_PTR( @info_ ) );
 end;
 
-procedure destroy_window( var info:T_sample_info );
+procedure destroy_window( var info_:T_sample_info );
 begin
-     vkDestroySurfaceKHR( info.inst, info.surface, nil );
-     DestroyWindow( info.window );
+     vkDestroySurfaceKHR( info_.inst, info_.surface, nil );
+     DestroyWindow( info_.window );
 end;
 
 //////////////////////////////////////////////////////////////////////////////// 06-init_depth_buffer
@@ -418,7 +418,7 @@ end;
 *)
 const PREFERRED_SURFACE_FORMAT = VK_FORMAT_B8G8R8A8_UNORM;
 
-procedure init_swapchain_extension( var info:T_sample_info );
+procedure init_swapchain_extension( var info_:T_sample_info );
 var
    res              :VkResult;
    createInfo       :VkWin32SurfaceCreateInfoKHR;
@@ -432,44 +432,44 @@ begin
      // Construct the surface description:
      createInfo.sType     := VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
      createInfo.pNext     := nil;
-     createInfo.hinstance := info.connection;
-     createInfo.hwnd      := info.window;
-     res := vkCreateWin32SurfaceKHR( info.inst, @createInfo, nil, @info.surface );
+     createInfo.hinstance := info_.connection;
+     createInfo.hwnd      := info_.window;
+     res := vkCreateWin32SurfaceKHR( info_.inst, @createInfo, nil, @info_.surface );
      Assert( res = VK_SUCCESS );
 
      // Iterate over each queue to learn whether it supports presenting:
-     SetLength( pSupportsPresent, info.queue_family_count );
-     for i := 0 to info.queue_family_count-1
-     do vkGetPhysicalDeviceSurfaceSupportKHR( info.gpus[0], i, info.surface, @pSupportsPresent[i] );
+     SetLength( pSupportsPresent, info_.queue_family_count );
+     for i := 0 to info_.queue_family_count-1
+     do vkGetPhysicalDeviceSurfaceSupportKHR( info_.gpus[0], i, info_.surface, @pSupportsPresent[i] );
 
      // Search for a graphics and a present queue in the array of queue
      // families, try to find one that supports both
-     info.graphics_queue_family_index := UINT32_MAX;
-     info.present_queue_family_index  := UINT32_MAX;
-     for i := 0 to info.queue_family_count-1 do
+     info_.graphics_queue_family_index := UINT32_MAX;
+     info_.present_queue_family_index  := UINT32_MAX;
+     for i := 0 to info_.queue_family_count-1 do
      begin
-          if ( info.queue_props[i].queueFlags and VkQueueFlags( VK_QUEUE_GRAPHICS_BIT ) ) <> 0 then
+          if ( info_.queue_props[i].queueFlags and VkQueueFlags( VK_QUEUE_GRAPHICS_BIT ) ) <> 0 then
           begin
-               if info.graphics_queue_family_index = UINT32_MAX then info.graphics_queue_family_index := i;
+               if info_.graphics_queue_family_index = UINT32_MAX then info_.graphics_queue_family_index := i;
 
                if pSupportsPresent[i] = VK_TRUE then
                begin
-                    info.graphics_queue_family_index := i;
-                    info.present_queue_family_index  := i;
+                    info_.graphics_queue_family_index := i;
+                    info_.present_queue_family_index  := i;
                     Break;
                end;
           end;
      end;
 
-     if info.present_queue_family_index = UINT32_MAX then
+     if info_.present_queue_family_index = UINT32_MAX then
      begin
           // If didn't find a queue that supports both graphics and present, then
           // find a separate present queue.
-          for i := 0 to info.queue_family_count-1 do
+          for i := 0 to info_.queue_family_count-1 do
           begin
                if pSupportsPresent[i] = VK_TRUE then
                begin
-                    info.present_queue_family_index := i;
+                    info_.present_queue_family_index := i;
                     Break;
                end;
           end;
@@ -478,29 +478,29 @@ begin
 
      // Generate error if could not find queues that support graphics
      // and present
-     if ( info.graphics_queue_family_index = UINT32_MAX ) or ( info.present_queue_family_index = UINT32_MAX ) then
+     if ( info_.graphics_queue_family_index = UINT32_MAX ) or ( info_.present_queue_family_index = UINT32_MAX ) then
      begin
           Log.d( 'Could not find a queues for both graphics and present' );
           RunError( 256-1 );
      end;
 
      // Get the list of VkFormats that are supported:
-     res := vkGetPhysicalDeviceSurfaceFormatsKHR( info.gpus[0], info.surface, @formatCount, nil );
+     res := vkGetPhysicalDeviceSurfaceFormatsKHR( info_.gpus[0], info_.surface, @formatCount, nil );
      Assert( res = VK_SUCCESS );
      SetLength( surfFormats, formatCount );
-     res := vkGetPhysicalDeviceSurfaceFormatsKHR( info.gpus[0], info.surface, @formatCount, @surfFormats[0] );
+     res := vkGetPhysicalDeviceSurfaceFormatsKHR( info_.gpus[0], info_.surface, @formatCount, @surfFormats[0] );
      Assert( res = VK_SUCCESS );
 
      // If the device supports our preferred surface format, use it.
      // Otherwise, use whatever the device's first reported surface
      // format is.
      Assert( formatCount >= 1 );
-     info.format := surfFormats[0].format;
+     info_.format := surfFormats[0].format;
      for i := 0 to formatCount-1 do
      begin
           if surfFormats[i].format = PREFERRED_SURFACE_FORMAT then
           begin
-               info.format := PREFERRED_SURFACE_FORMAT;
+               info_.format := PREFERRED_SURFACE_FORMAT;
                break;
           end;
      end;
