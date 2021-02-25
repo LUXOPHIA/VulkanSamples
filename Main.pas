6370c0461168445d8f9ce546102153c6b648247e
luxidea <contact@luxidea.net>
2021-02-25 14:39:51 +0900

    14-init_pipeline

--- Main.pas
+++ Main.pas
@@ -1,12 +1,12 @@
 ﻿unit Main;
 
-{ https://github.com/LunarG/VulkanSamples/tree/master/API-Samples/13-init_vertex_buffer }
+{ https://github.com/LunarG/VulkanSamples/tree/master/API-Samples/14-init_pipeline }
 
 (*
  * Vulkan Samples
  *
- * Copyright (C) 2015-2016 Valve Corporation
- * Copyright (C) 2015-2016 LunarG, Inc.
+ * Copyright (C) 2015-2020 Valve Corporation
+ * Copyright (C) 2015-2020 LunarG, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,10 +23,15 @@
 
 (*
 VULKAN_SAMPLE_SHORT_DESCRIPTION
-Create Vertex Buffer
+Create Graphics Pipeline
 *)
 
 (* This is part of the draw cube progression *)
+
+(* We've setup cmake to process 14-init_pipeline.vert and 14-init_pipeline.frag           *)
+(* files containing the glsl shader code for this sample.  The generate-spirv script uses *)
+(* glslangValidator to compile the glsl into spir-v and places the spir-v into a struct   *)
+(* into a generated header file                                                           *)
 
 interface //#################################################################### ■
 
@@ -45,11 +50,10 @@ type
     procedure FormDestroy(Sender: TObject);
   private
     { private 宣言 }
-    const sample_title = 'Vertex Buffer Sample';
+    const sample_title = 'Graphics Pipeline Sample';
   public
     { public 宣言 }
-    info                   :T_sample_info;
-    imageAcquiredSemaphore :VkSemaphore;
+    info :T_sample_info;
   end;
 
 var
@@ -59,24 +63,32 @@ implementation //###############################################################
 
 {$R *.fmx}
 
-uses WinApi.Windows,
-     cube_data;
+uses cube_data;
 
 procedure TForm1.FormCreate(Sender: TObject);
 const
      depthPresent :T_bool = True;
 var
-   res                              :VkResult;
-   pass                             :T_bool;
-   buf_info                         :VkBufferCreateInfo;
-   mem_reqs                         :VkMemoryRequirements;
-   alloc_info                       :VkMemoryAllocateInfo;
-   pData                            :P_uint8_t;
-   offsets                          :array [ 0..1-1 ] of VkDeviceSize;
-   clear_values                     :array [ 0..2-1 ] of VkClearValue;
-   imageAcquiredSemaphoreCreateInfo :VkSemaphoreCreateInfo;
-   rp_begin                         :VkRenderPassBeginInfo;
+   res                  :VkResult;
+   __init_pipeline_vert :TMemoryStream;
+   __init_pipeline_frag :TMemoryStream;
+   vert_info            :VkShaderModuleCreateInfo;
+   frag_info            :VkShaderModuleCreateInfo;
+   dynamicStateEnables  :array [ 0..2-1 ] of VkDynamicState;  // Viewport + Scissor
+   dynamicState         :VkPipelineDynamicStateCreateInfo;
+   vi                   :VkPipelineVertexInputStateCreateInfo;
+   ia                   :VkPipelineInputAssemblyStateCreateInfo;
+   rs                   :VkPipelineRasterizationStateCreateInfo;
+   cb                   :VkPipelineColorBlendStateCreateInfo;
+   att_state            :array [ 0..1-1 ] of VkPipelineColorBlendAttachmentState;
+   vp                   :VkPipelineViewportStateCreateInfo;
+   ds                   :VkPipelineDepthStencilStateCreateInfo;
+   ms                   :VkPipelineMultisampleStateCreateInfo;
+   pipeline             :VkGraphicsPipelineCreateInfo;
 begin
+     __init_pipeline_vert := TMemoryStream.Create;
+     __init_pipeline_frag := TMemoryStream.Create;
+
      init_global_layer_properties( info );
      init_instance_extension_names( info );
      init_device_extension_names( info );
@@ -93,125 +105,157 @@ begin
      init_device_queue( info );
      init_swap_chain( info );
      init_depth_buffer( info );
+     init_uniform_buffer( info );
      init_renderpass( info, depthPresent );
      init_framebuffers( info, depthPresent );
+     init_vertex_buffer( info, @g_vb_solid_face_colors_Data[0], sizeof(g_vb_solid_face_colors_Data ),
+                         SizeOf( T_Vertex ) * Length( g_vb_solid_face_colors_Data ), False );
+     init_descriptor_and_pipeline_layouts( info, false );
+     init_descriptor_pool( info, false );
+     init_descriptor_set( info, false );
+     __init_pipeline_vert.LoadFromFile( '../../_DATA/14-init_pipeline.vert' );
+     __init_pipeline_frag.LoadFromFile( '../../_DATA/14-init_pipeline.frag' );
+     vert_info.sType    := VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+     frag_info.sType    := VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+     vert_info.codeSize := __init_pipeline_vert.Size;
+     vert_info.pCode    := __init_pipeline_vert.Memory;
+     frag_info.codeSize := __init_pipeline_frag.Size;
+     frag_info.pCode    := __init_pipeline_frag.Memory;
+     init_shaders( info, @vert_info, @frag_info );
 
      (* VULKAN_KEY_START *)
-     (*
-      * Set up a vertex buffer:
-      * - Create a buffer
-      * - Map it and write the vertex data into it
-      * - Bind it using vkCmdBindVertexBuffers
-      * - Later, at pipeline creation,
-      * -      fill in vertex input part of the pipeline with relevent data
-      *)
+     dynamicState.sType             := VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+     dynamicState.pNext             := nil;
+     dynamicState.pDynamicStates    := @dynamicStateEnables[0];
+     dynamicState.dynamicStateCount := 0;
 
-     buf_info.sType                 := VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-     buf_info.pNext                 := nil;
-     buf_info.usage                 := Ord( VK_BUFFER_USAGE_VERTEX_BUFFER_BIT );
-     buf_info.size                  := SizeOf( T_Vertex ) * Length( g_vb_solid_face_colors_Data );
-     buf_info.queueFamilyIndexCount := 0;
-     buf_info.pQueueFamilyIndices   := nil;
-     buf_info.sharingMode           := VK_SHARING_MODE_EXCLUSIVE;
-     buf_info.flags                 := 0;
-     res := vkCreateBuffer( info.device, @buf_info, nil, @info.vertex_buffer.buf );
-     Assert( res = VK_SUCCESS, 'vkCreateBuffer' );
+     vi.sType                           := VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+     vi.pNext                           := nil;
+     vi.flags                           := 0;
+     vi.vertexBindingDescriptionCount   := 1;
+     vi.pVertexBindingDescriptions      := @info.vi_binding;
+     vi.vertexAttributeDescriptionCount := 2;
+     vi.pVertexAttributeDescriptions    := @info.vi_attribs[0];
 
-     vkGetBufferMemoryRequirements( info.device, info.vertex_buffer.buf, @mem_reqs );
+     ia.sType                  := VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+     ia.pNext                  := nil;
+     ia.flags                  := 0;
+     ia.primitiveRestartEnable := VK_FALSE;
+     ia.topology               := VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
 
-     alloc_info.sType := VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-     alloc_info.pNext := nil;
-     alloc_info.memoryTypeIndex := 0;
+     rs.sType                   := VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+     rs.pNext                   := nil;
+     rs.flags                   := 0;
+     rs.polygonMode             := VK_POLYGON_MODE_FILL;
+     rs.cullMode                := Ord( VK_CULL_MODE_BACK_BIT );
+     rs.frontFace               := VK_FRONT_FACE_CLOCKWISE;
+     rs.depthClampEnable        := VK_FALSE;
+     rs.rasterizerDiscardEnable := VK_FALSE;
+     rs.depthBiasEnable         := VK_FALSE;
+     rs.depthBiasConstantFactor := 0;
+     rs.depthBiasClamp          := 0;
+     rs.depthBiasSlopeFactor    := 0;
+     rs.lineWidth               := 1.0;
 
-     alloc_info.allocationSize := mem_reqs.size;
-     pass := memory_type_from_properties( info, mem_reqs.memoryTypeBits,
-                                          Ord( VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT ) or Ord( VK_MEMORY_PROPERTY_HOST_COHERENT_BIT ),
-                                          alloc_info.memoryTypeIndex );
-     Assert( pass, 'No mappable, coherent memory' );
+     cb.sType := VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+     cb.pNext := nil;
+     cb.flags := 0;
+     att_state[0].colorWriteMask      := $f;
+     att_state[0].blendEnable         := VK_FALSE;
+     att_state[0].alphaBlendOp        := VK_BLEND_OP_ADD;
+     att_state[0].colorBlendOp        := VK_BLEND_OP_ADD;
+     att_state[0].srcColorBlendFactor := VK_BLEND_FACTOR_ZERO;
+     att_state[0].dstColorBlendFactor := VK_BLEND_FACTOR_ZERO;
+     att_state[0].srcAlphaBlendFactor := VK_BLEND_FACTOR_ZERO;
+     att_state[0].dstAlphaBlendFactor := VK_BLEND_FACTOR_ZERO;
+     cb.attachmentCount   := 1;
+     cb.pAttachments      := @att_state[0];
+     cb.logicOpEnable     := VK_FALSE;
+     cb.logicOp           := VK_LOGIC_OP_NO_OP;
+     cb.blendConstants[0] := 1.0;
+     cb.blendConstants[1] := 1.0;
+     cb.blendConstants[2] := 1.0;
+     cb.blendConstants[3] := 1.0;
 
-     res := vkAllocateMemory( info.device, @alloc_info, nil, @info.vertex_buffer.mem );
-     Assert( res = VK_SUCCESS );
+     vp.sType         := VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+     vp.pNext         := nil;
+     vp.flags         := 0;
+     vp.viewportCount := NUM_VIEWPORTS;
+     dynamicStateEnables[dynamicState.dynamicStateCount] := VK_DYNAMIC_STATE_VIEWPORT;  Inc( dynamicState.dynamicStateCount );
+     vp.scissorCount  := NUM_SCISSORS;
+     dynamicStateEnables[dynamicState.dynamicStateCount] := VK_DYNAMIC_STATE_SCISSOR ;  Inc( dynamicState.dynamicStateCount );
+     vp.pScissors     := nil;
+     vp.pViewports    := nil;
 
-     res := vkMapMemory( info.device, info.vertex_buffer.mem, 0, mem_reqs.size, 0, @pData );
-     Assert( res = VK_SUCCESS, 'vkMapMemory' );
+     ds.sType                 := VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+     ds.pNext                 := nil;
+     ds.flags                 := 0;
+     ds.depthTestEnable       := VK_TRUE;
+     ds.depthWriteEnable      := VK_TRUE;
+     ds.depthCompareOp        := VK_COMPARE_OP_LESS_OR_EQUAL;
+     ds.depthBoundsTestEnable := VK_FALSE;
+     ds.minDepthBounds        := 0;
+     ds.maxDepthBounds        := 0;
+     ds.stencilTestEnable     := VK_FALSE;
+     ds.back.failOp           := VK_STENCIL_OP_KEEP;
+     ds.back.passOp           := VK_STENCIL_OP_KEEP;
+     ds.back.compareOp        := VK_COMPARE_OP_ALWAYS;
+     ds.back.compareMask      := 0;
+     ds.back.reference        := 0;
+     ds.back.depthFailOp      := VK_STENCIL_OP_KEEP;
+     ds.back.writeMask        := 0;
+     ds.front                 := ds.back;
 
-     Move( g_vb_solid_face_colors_Data[0], pData^, SizeOf( T_Vertex ) * Length( g_vb_solid_face_colors_Data ) );
+     ms.sType                 := VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+     ms.pNext                 := nil;
+     ms.flags                 := 0;
+     ms.pSampleMask           := nil;
+     ms.rasterizationSamples  := NUM_SAMPLES;
+     ms.sampleShadingEnable   := VK_FALSE;
+     ms.alphaToCoverageEnable := VK_FALSE;
+     ms.alphaToOneEnable      := VK_FALSE;
+     ms.minSampleShading      := 0.0;
 
-     vkUnmapMemory( info.device, info.vertex_buffer.mem );
+     pipeline.sType               := VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+     pipeline.pNext               := nil;
+     pipeline.layout              := info.pipeline_layout;
+     pipeline.basePipelineHandle  := VK_NULL_HANDLE;
+     pipeline.basePipelineIndex   := 0;
+     pipeline.flags               := 0;
+     pipeline.pVertexInputState   := @vi;
+     pipeline.pInputAssemblyState := @ia;
+     pipeline.pRasterizationState := @rs;
+     pipeline.pColorBlendState    := @cb;
+     pipeline.pTessellationState  := nil;
+     pipeline.pMultisampleState   := @ms;
+     pipeline.pDynamicState       := @dynamicState;
+     pipeline.pViewportState      := @vp;
+     pipeline.pDepthStencilState  := @ds;
+     pipeline.pStages             := @info.shaderStages[0];
+     pipeline.stageCount          := 2;
+     pipeline.renderPass          := info.render_pass;
+     pipeline.subpass             := 0;
 
-     res := vkBindBufferMemory( info.device, info.vertex_buffer.buf, info.vertex_buffer.mem, 0 );
-     Assert( res = VK_SUCCESS, 'vkBindBufferMemory' );
-
-     (* We won't use these here, but we will need this info when creating the
-     * pipeline *)
-     info.vi_binding.binding   := 0;
-     info.vi_binding.inputRate := VK_VERTEX_INPUT_RATE_VERTEX;
-     info.vi_binding.stride    := SizeOf( g_vb_solid_face_colors_Data[0] );
-
-     info.vi_attribs[0].binding  := 0;
-     info.vi_attribs[0].location := 0;
-     info.vi_attribs[0].format   := VK_FORMAT_R32G32B32A32_SFLOAT;
-     info.vi_attribs[0].offset   := 0;
-     info.vi_attribs[1].binding  := 0;
-     info.vi_attribs[1].location := 1;
-     info.vi_attribs[1].format   := VK_FORMAT_R32G32B32A32_SFLOAT;
-     info.vi_attribs[1].offset   := 16;
-
-     offsets[0] := 0;
-
-     (* We cannot bind the vertex buffer until we begin a renderpass *)
-     clear_values[0].color.float32[0]     := 0.2;
-     clear_values[0].color.float32[1]     := 0.2;
-     clear_values[0].color.float32[2]     := 0.2;
-     clear_values[0].color.float32[3]     := 0.2;
-     clear_values[1].depthStencil.depth   := 1.0;
-     clear_values[1].depthStencil.stencil := 0;
-
-     imageAcquiredSemaphoreCreateInfo.sType := VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
-     imageAcquiredSemaphoreCreateInfo.pNext := nil;
-     imageAcquiredSemaphoreCreateInfo.flags := 0;
-
-     res := vkCreateSemaphore( info.device, @imageAcquiredSemaphoreCreateInfo, nil, @imageAcquiredSemaphore );
-     Assert( res = VK_SUCCESS, 'vkCreateSemaphore' );
-
-     // Get the index of the next available swapchain image:
-     res := vkAcquireNextImageKHR( info.device, info.swap_chain, UINT64_MAX, imageAcquiredSemaphore, VK_NULL_HANDLE,
-                                   @info.current_buffer );
-     // TODO: Deal with the VK_SUBOPTIMAL_KHR and VK_ERROR_OUT_OF_DATE_KHR
-     // return codes
-     Assert( res = VK_SUCCESS, 'vkAcquireNextImageKHR' );
-
-     rp_begin.sType                    := VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-     rp_begin.pNext                    := nil;
-     rp_begin.renderPass               := info.render_pass;
-     rp_begin.framebuffer              := info.framebuffers[info.current_buffer];
-     rp_begin.renderArea.offset.x      := 0;
-     rp_begin.renderArea.offset.y      := 0;
-     rp_begin.renderArea.extent.width  := info.width;
-     rp_begin.renderArea.extent.height := info.height;
-     rp_begin.clearValueCount          := 2;
-     rp_begin.pClearValues             := @clear_values[0];
-
-     vkCmdBeginRenderPass( info.cmd, @rp_begin, VK_SUBPASS_CONTENTS_INLINE );   { TODO : ACCESS_VIOLATION @ nvoglv64.dll }
-
-     vkCmdBindVertexBuffers( info.cmd, 0,              (* Start Binding *)
-                             1,                        (* Binding Count *)
-                             @info.vertex_buffer.buf,  (* pBuffers      *)
-                             @offsets[0] );            (* pOffsets      *)
-
-     vkCmdEndRenderPass( info.cmd );
+     res := vkCreateGraphicsPipelines( info.device, VK_NULL_HANDLE, 1, @pipeline, nil, @info.pipeline );
+     Assert( res = VK_SUCCESS, 'vkCreateGraphicsPipelines' );
      execute_end_command_buffer( info );
      execute_queue_command_buffer( info );
      (* VULKAN_KEY_END *)
+
+     __init_pipeline_vert.Free;
+     __init_pipeline_frag.Free;
 end;
 
-procedure TForm1.FormDestroy( Sender: TObject );
+procedure TForm1.FormDestroy(Sender: TObject);
 begin
-     vkDestroySemaphore( info.device, imageAcquiredSemaphore, nil );
-     vkDestroyBuffer( info.device, info.vertex_buffer.buf, nil );
-     vkFreeMemory( info.device, info.vertex_buffer.mem, nil );
+     vkDestroyPipeline( info.device, info.pipeline, nil );
+     destroy_descriptor_pool( info );
+     destroy_vertex_buffer( info );
      destroy_framebuffers( info );
+     destroy_shaders( info );
      destroy_renderpass( info );
+     destroy_descriptor_and_pipeline_layouts( info );
+     destroy_uniform_buffer( info );
      destroy_depth_buffer( info );
      destroy_swap_chain( info );
      destroy_command_buffer( info );
