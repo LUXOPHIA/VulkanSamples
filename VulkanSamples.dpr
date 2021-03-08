f95daf6a04247e9a5a73377f6a3cd33225f450dd
luxidea <contact@luxidea.net>
2021-03-08 23:29:45 +0900

    no message

--- VulkanSamples.dpr
+++ VulkanSamples.dpr
@@ -29,7 +29,7 @@ uses
   vulkan.util_init in '_LIBRARY\vulkan.util_init.pas',
   vulkan.util in '_LIBRARY\vulkan.util.pas',
   LUX.GPU.Vulkan.root in '_LIBRARY\LUXOPHIA\LUX.GPU.Vulkan\LUX.GPU.Vulkan.root.pas',
-  LUX.GPU.Vulkan.Instance in '_LIBRARY\LUXOPHIA\LUX.GPU.Vulkan\LUX.GPU.Vulkan.Instance.pas',
+  LUX.GPU.Vulkan.Instan in '_LIBRARY\LUXOPHIA\LUX.GPU.Vulkan\LUX.GPU.Vulkan.Instan.pas',
   LUX.GPU.Vulkan.Window in '_LIBRARY\LUXOPHIA\LUX.GPU.Vulkan\LUX.GPU.Vulkan.Window.pas',
   LUX.GPU.Vulkan.Surface in '_LIBRARY\LUXOPHIA\LUX.GPU.Vulkan\LUX.GPU.Vulkan.Surface.pas',
   LUX.GPU.Vulkan.Command in '_LIBRARY\LUXOPHIA\LUX.GPU.Vulkan\LUX.GPU.Vulkan.Command.pas',
