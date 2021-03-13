90d277b5adeca919862b6aac3210b4871c3eab17
luxidea <contact@luxidea.net>
2021-03-14 02:14:07 +0900

    TVkDeviceLister クラスを追加。

--- _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/LUX.GPU.Vulkan.root.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/LUX.GPU.Vulkan.root.pas
@@ -2,7 +2,14 @@
 
 interface //#################################################################### ■
 
+uses SYstem.Generics.Collections;
+
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TVkDeviceChildr<TVkDevice_,TVkParent_:class>            = class;
+
+     TVkDeviceLister<TVkDevice_,TVkParent_,TVkChildr_:class> = class;
+     TVkDeviceLister<TVkDevice_,TVkChildr_:class>            = class;
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
@@ -20,10 +27,36 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      public
        constructor Create; overload; virtual;
        constructor Create( const Parent_:TVkParent_ ); overload; virtual;
-       destructor Destroy; override;
        ///// プロパティ
        property Device :TVkDevice_ read GetDevice;
        property Parent :TVkParent_ read GetParent;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkDeviceLister<TVkDevice_,TVkParent_,TVkChildr_>
+
+     TVkDeviceLister<TVkDevice_,TVkParent_,TVkChildr_:class> = class( TObjectList<TVkChildr_> )
+     private
+     protected
+       _Parent :TVkParent_;
+       ///// アクセス
+       function GetDevice :TVkDevice_; virtual; abstract;
+       function GetParent :TVkParent_; virtual;
+     public
+       constructor Create; overload; virtual;
+       constructor Create( const Parent_:TVkParent_ ); overload; virtual;
+       ///// プロパティ
+       property Device :TVkDevice_ read GetDevice;
+       property Parent :TVkParent_ read GetParent;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkDeviceLister<TVkDevice_,TVkChildr_>
+
+     TVkDeviceLister<TVkDevice_,TVkChildr_:class> = class( TVkDeviceLister<TVkDevice_,TVkDevice_,TVkChildr_> )
+     private
+     protected
+       ///// アクセス
+       function GetDevice :TVkDevice_; override;
+     public
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -64,11 +97,46 @@ begin
      _Parent := Parent_;
 end;
 
-destructor TVkDeviceChildr<TVkDevice_,TVkParent_>.Destroy;
-begin
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkDeviceLister<TVkDevice_,TVkParent_,TVkChildr_>
 
-     inherited;
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+function TVkDeviceLister<TVkDevice_,TVkParent_,TVkChildr_>.GetParent :TVkParent_;
+begin
+     Result := _Parent;
 end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TVkDeviceLister<TVkDevice_,TVkParent_,TVkChildr_>.Create;
+begin
+     inherited;
+
+end;
+
+constructor TVkDeviceLister<TVkDevice_,TVkParent_,TVkChildr_>.Create( const Parent_:TVkParent_ );
+begin
+     Create;
+
+     _Parent := Parent_;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkDeviceLister<TVkDevice_,TVkChildr_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TVkDeviceLister<TVkDevice_,TVkChildr_>.GetDevice :TVkDevice_;
+begin
+     Result := _Parent;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
