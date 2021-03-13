cc63637a80c25d8bd1ffa6996b4a95afc3cecce0
luxidea <contact@luxidea.net>
2021-03-14 00:49:52 +0900

    TVkDeviceChildr クラスを追加。

---
+++ _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/LUX.GPU.Vulkan.root.pas
@@ -0,0 +1,75 @@
+﻿unit LUX.GPU.Vulkan.root;
+
+interface //#################################################################### ■
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkDeviceChildr<TVkDevice_,TVkParent_>
+
+     TVkDeviceChildr<TVkDevice_,TVkParent_:class> = class
+     private
+     protected
+       _Parent :TVkParent_;
+       ///// アクセス
+       function GetDevice :TVkDevice_; virtual; abstract;
+       function GetParent :TVkParent_;                                          { TODO : virtual; にすると「F2084 内部エラー: URW1302」が生じる。 }
+     public
+       constructor Create; overload; virtual;
+       constructor Create( const Parent_:TVkParent_ ); overload; virtual;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Device :TVkDevice_ read GetDevice;
+       property Parent :TVkParent_ read GetParent;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkDeviceChildr<TVkDevice_,TVkParent_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+function TVkDeviceChildr<TVkDevice_,TVkParent_>.GetParent :TVkParent_;
+begin
+     Result := _Parent;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TVkDeviceChildr<TVkDevice_,TVkParent_>.Create;
+begin
+     inherited;
+
+end;
+
+constructor TVkDeviceChildr<TVkDevice_,TVkParent_>.Create( const Parent_:TVkParent_ );
+begin
+     Create;
+
+     _Parent := Parent_;
+end;
+
+destructor TVkDeviceChildr<TVkDevice_,TVkParent_>.Destroy;
+begin
+
+     inherited;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+end. //######################################################################### ■
