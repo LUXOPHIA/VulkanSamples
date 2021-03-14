b1a7d757583feedeab6eaea235d0365f7bbdeef3
luxidea <contact@luxidea.net>
2021-03-14 20:19:40 +0900

    TVkUniBuf クラスを LUX.GPU.Vulkan.Buffer.Unifor ユニットへ分離。

---
+++ _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/LUX.GPU.Vulkan.Buffer.Unifor.pas
@@ -0,0 +1,109 @@
+﻿unit LUX.GPU.Vulkan.Buffer.Unifor;
+
+interface //#################################################################### ■
+
+uses vulkan_core,
+     LUX.GPU.Vulkan.Buffer;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TVkUniBuf<TVkDevice_:class>                = class;
+     TVkUniBuf<TVkDevice_:class;TValue_:record> = class;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkUniBuf<TVkDevice_>
+
+     TVkUniBuf<TVkDevice_:class> = class( TVkBuffer<TVkDevice_> )
+     private
+     protected
+     public
+       constructor Create; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkBuffer<TVkDevice_,TValue_>
+
+     TVkUniBuf<TVkDevice_:class;TValue_:record> = class( TVkUniBuf<TVkDevice_> )
+     private
+     protected
+       _Value :TValue_;
+       ///// アクセス
+       function GetValue :TValue_; virtual;
+       procedure SetValue( const Value_:TValue_ ); virtual;
+       ///// プロパティ
+       property Size :VkDeviceSize read GetSize write SetSize;
+     public
+       constructor Create; override;
+       ///// プロパティ
+       property Value :TValue_ read GetValue write SetValue;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses LUX.GPU.Vulkan;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkUniBuf<TVkDevice_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TVkUniBuf<TVkDevice_>.Create;
+begin
+     inherited;
+
+     Usage := Ord( VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT );
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkBuffer<TVkDevice_,TValue_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TVkUniBuf<TVkDevice_,TValue_>.GetValue :TValue_;
+begin
+     Result := _Value;
+end;
+
+procedure TVkUniBuf<TVkDevice_,TValue_>.SetValue( const Value_:TValue_ );
+var
+   P :Pointer;
+begin
+     _Value := Value_;
+
+     _Memory.Map( P );
+
+     Move( _Value, P^, Size );
+
+     _Memory.Unmap;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TVkUniBuf<TVkDevice_,TValue_>.Create;
+begin
+     inherited;
+
+     Size := SizeOf( TValue_ );
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+end. //######################################################################### ■
--- _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/LUX.GPU.Vulkan.Buffer.pas
+++ _LIBRARY/LUXOPHIA/LUX.GPU.Vulkan/LUX.GPU.Vulkan.Buffer.pas
@@ -12,9 +12,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        TVkBuffer<TVkDevice_,TVkParent_:class>   = class;
          TVkBufMem<TVkDevice_,TVkParent_:class> = class;
 
-       TVkBuffer<TVkDevice_:class>                = class;
-       TVkUniBuf<TVkDevice_:class>                = class;
-       TVkUniBuf<TVkDevice_:class;TValue_:record> = class;
+       TVkBuffer<TVkDevice_:class>              = class;
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
@@ -91,32 +89,6 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        constructor Create( const Device_:TVkDevice_ ); overload; virtual;
        ///// プロパティ
        property Buffers :TVkBuffers_ read GetParent;
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkUniBuf<TVkDevice_>
-
-     TVkUniBuf<TVkDevice_:class> = class( TVkBuffer<TVkDevice_> )
-     private
-     protected
-     public
-       constructor Create; override;
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkBuffer<TVkDevice_,TValue_>
-
-     TVkUniBuf<TVkDevice_:class;TValue_:record> = class( TVkUniBuf<TVkDevice_> )
-     private
-     protected
-       _Value :TValue_;
-       ///// アクセス
-       function GetValue :TValue_; virtual;
-       procedure SetValue( const Value_:TValue_ ); virtual;
-       ///// プロパティ
-       property Size :VkDeviceSize read GetSize write SetSize;
-     public
-       constructor Create; override;
-       ///// プロパティ
-       property Value :TValue_ read GetValue write SetValue;
      end;
 
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkBuffers<TVkDevice_>
@@ -354,56 +326,6 @@ end;
 constructor TVkBuffer<TVkDevice_>.Create( const Device_:TVkDevice_ );
 begin
      Create( TVkBuffers_( TVkDevice( Device_ ).Buffers ) );
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkUniBuf<TVkDevice_>
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-constructor TVkUniBuf<TVkDevice_>.Create;
-begin
-     inherited;
-
-     Usage := Ord( VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT );
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkBuffer<TVkDevice_,TValue_>
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-/////////////////////////////////////////////////////////////////////// アクセス
-
-function TVkUniBuf<TVkDevice_,TValue_>.GetValue :TValue_;
-begin
-     Result := _Value;
-end;
-
-procedure TVkUniBuf<TVkDevice_,TValue_>.SetValue( const Value_:TValue_ );
-var
-   P :Pointer;
-begin
-     _Value := Value_;
-
-     _Memory.Map( P );
-
-     Move( _Value, P^, Size );
-
-     _Memory.Unmap;
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-constructor TVkUniBuf<TVkDevice_,TValue_>.Create;
-begin
-     inherited;
-
-     Size := SizeOf( TValue_ );
 end;
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TVkBuffers<TVkDevice_>
